7516dd189991fec1aa4373f9165964b5d2b7fc3e
Daniel Sikar <dsikar@gmail.com>
2018-09-20 11:21:57 +0100

    Added wiring schematic

--- telemetry/telemetry.ino
+++ telemetry/telemetry.ino
@@ -1,21 +1,45 @@
+/*
+      Wiring
+ 
+      433MHz 100mw
+       RF modules                                       NodeMCU
+      +-----------+                                  +----------+
+      |        TX +------+ RX (PIN 5)    TX   +------+ PIN 5    |
+      |           |                                  |          |
+      |        RX +------+ TX (PIN 4)    RX   +------+ PIN 4    |
+      |           |                                  |      USB +--------+ TERMINAL
+      |        5V +------+ 5V            3.3V +------+ VIN      |
+      |           |                                  |          |
+      |        GND+------+ GND           GND  +------+ GND      |
+      +-----------+                                  +----------+
+      +-----------+                                  +----------+
+      |        TX +------+ RX (PIN 5)    TX   +------+ PIN 5    |
+      |           |                                  |          |
+      |        RX +------+ TX (PIN 4)    RX   +------+ PIN 4    |
+      |           |                                  |      USB +--------+ TERMINAL
+      |        5V +------+ 5V            3.3V +------+ VIN      |
+      |           |                                  |          |
+      |        GND+------+ GND           GND  +------+ GND      |
+      +-----------+                                  +----------+
+ */
+
 #include <SoftwareSerial.h>
 
 SoftwareSerial mySerial(5, 4); // RX, TX
 
 void setup() {
   // Open serial communications and wait for port to open:
-  Serial.begin(115200);
+  Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
   Serial.println("Formula Telemetry v0.1");
-  Serial.println("COM Baud rate 115200");
+  Serial.println("COM Baud rate 9600");
   Serial.println("RF Baud rate 57600");
 
   // set the data rate for the SoftwareSerial port
   mySerial.begin(57600);
-  /// mySerial.println("Hello, world?");
 }
 
 void loop() { // run over and over
